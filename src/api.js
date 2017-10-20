0c6183ce63bf50e98cbf1df8b58489df53240c92
Marcel Zanluca <marcel.zanluca@gmail.com>
2017-10-20 01:32:20 -0200

    endpoint messages

--- src/api.js
+++ src/api.js
@@ -3,8 +3,8 @@ import { Router } from 'express'
 const router = new Router()
 
 router.get('', (request, response) => response.json([]))
-router.post('', (request, response) => response.sendStatus(204))
-router.put('/:id', (request, response) => response.sendStatus(204))
-router.delete('/:id', (request, response) => response.sendStatus(204))
+router.post('', (request, response) => response.send('Você inseriu um novo registro!'))
+router.put('/:id', (request, response) => response.send(`Você atualizou o registro de id: ${request.params.id}`))
+router.delete('/:id', (request, response) => response.send(`Você excluiu o registro de id: ${request.params.id}`))
 
 export default router
