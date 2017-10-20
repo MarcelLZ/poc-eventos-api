a9e81f802b8475e9909a14ae9adf7b8183090bff
vmarcosp <contato@vmarcosp.com.br>
2017-10-20 01:48:54 -0200

    Add mongoose and bluebird, created mongoose connection :coffee:

---
+++ src/events/model.js
@@ -0,0 +1,37 @@
+import mongoose from '../database';
+
+// Event model - Simple schema and no validations 
+const eventModel = mongoose.model('events', {
+  "title": String,
+  "date": Object,
+  "innerLink": String,
+  "link": String,
+  "price": String,
+  "location": Object,
+  "image": String,
+  "shortDescription": String,
+});
+
+export default eventModel;
+/**
+ * Example
+ * {
+  "title": "2º Vue.js Floripa",
+  "date": {
+    "day": "18",
+    "month": "Outubro",
+    "year": 2017
+  },
+  "innerLink": "inner.html",
+  "link": "https://www.meetup.com/pt-BR/floripa-vuejs/events/243932293/",
+  "price": "Free",
+  "location": {
+    "city": "Florianópolis",
+    "state": "SC",
+    "address": "Chaordic",
+    "locationUrl": "https://maps.google.com/maps?f=q&hl=en&q=Av.+Afonso+Delambert+Neto%2C+26+-+Lagoa+da+Concei%C3%A7%C3%A3o%2C+Florian%C3%B3polis%2C+br"
+  },
+  "image": "https://secure.meetupstatic.com/photos/theme_head/8/a/6/a/full_7535434.jpeg",
+  "shortDescription": "Agora dia 18 de outubro a partir das 19:30h na Chaordic vai rolar o segundo encontro do Vue.js Floripa! Seja você desenvolvedor, frontend, backend, designer ou amante de tecnologia, contamos com a sua presença! Lembrando a todos que o evento é gratuito."
+}
+ */
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
 import api from './api'
 
 const PORT = process.env.PORT || 3000
@@ -12,3 +11,4 @@ express()
   .use('/eventos', api)
   .get('/', (req, res) => res.json({ healthCheck: true }))
   .listen(PORT, () => console.log(`Running! Access http://localhost:${PORT}`))
+
