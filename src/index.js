a6c499252c2e03182dfe80437f2f2a3eee26e76b
Marcel Zanluca <marcel.zanluca@gmail.com>
2017-10-20 01:11:09 -0200

    now health check route

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const PORT = process.env.PORT || 3000
 express()
   .use('*', cors({ origin: '*' }))
   .use(bodyParser.json())
-  .use('/', api)
-  .get('/health-check', (req, res) => res.json({ healthCheck: true }))
+  .use('/eventos', api)
+  .get('/', (req, res) => res.json({ healthCheck: true }))
   .listen(PORT, () => console.log(`Running! Access http://localhost:${PORT}`))
